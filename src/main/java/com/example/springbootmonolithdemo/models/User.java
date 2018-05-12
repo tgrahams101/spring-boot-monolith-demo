0ae23394bd4c62ea132c70bea62c4f90becac168
Ted Anyansi <tanyansi@gmail.com>
2018-05-12 16:21:57 -0700

    created User Repository

---
+++ src/main/java/com/example/springbootmonolithdemo/models/User.java
@@ -0,0 +1,56 @@
+package com.example.springbootmonolithdemo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USER_NAME")
+    private String userName;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
