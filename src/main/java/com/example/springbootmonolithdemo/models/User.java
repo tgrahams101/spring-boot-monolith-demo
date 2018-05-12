a6e96d45ab6d485e0f8529b00ed1772d1b366d49
Ted Anyansi <tanyansi@gmail.com>
2018-05-12 16:33:19 -0700

    refactored User model to use Lombok for abbreviated code for getters and setters in model

--- src/main/java/com/example/springbootmonolithdemo/models/User.java
+++ src/main/java/com/example/springbootmonolithdemo/models/User.java
@@ -1,13 +1,15 @@
 package com.example.springbootmonolithdemo.models;
 
+import lombok.*;
 import javax.persistence.*;
 
-@Entity
-@Table(name = "USERS")
+@Data
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+@Entity @Table(name = "USERS")
 public class User {
 
     @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @GeneratedValue(strategy = GenerationType.IDENTIY)
     private Long id;
 
     @Column(name = "USER_NAME")
@@ -19,38 +21,57 @@ public class User {
     @Column(name = "LAST_NAME")
     private String lastName;
 
-    public User() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return userName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
 }
+
+//@Entity
+//@Table(name = "USERS")
+//public class User {
+//
+//    @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "USER_NAME")
+//    private String userName;
+//
+//    @Column(name = "FIRST_NAME")
+//    private String firstName;
+//
+//    @Column(name = "LAST_NAME")
+//    private String lastName;
+//
+//    public User() {}
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getUsername() {
+//        return userName;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//}
