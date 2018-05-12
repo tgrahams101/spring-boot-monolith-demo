81008deecc6fd238999d8ad9476542d794b60530
Ted Anyansi <tanyansi@gmail.com>
2018-05-12 01:52:25 -0700

    added a second migration file that adds first & last name columns to user table, and modified seed file to insert user rows with corresponding additional information

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS
+	(USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+	('user from seeds', 'GUCCI', 'Mane'),
+	('another user', 'Don', 'King'),
+	('someone', 'Han', 'Jin'),
+	('someone else', 'Bruce', 'Wayne');
