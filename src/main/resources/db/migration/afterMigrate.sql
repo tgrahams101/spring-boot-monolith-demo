943a467db925bca4b108785a5fb54a3f22c681f3
Ted Anyansi <tanyansi@gmail.com>
2018-05-12 19:37:58 -0700

    added new songs table migration file and a seed of songs in the afterMigrate sql file

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -5,3 +5,11 @@ VALUES
 	('another user', 'Don', 'King'),
 	('someone', 'Han', 'Jin'),
 	('someone else', 'Bruce', 'Wayne');
+
+INSERT INTO SONGS
+  (TITLE, LENGTH)
+VALUES
+  ('Premier Gao', 2.33),
+  ('Delicate', 4.34),
+  ('Electric Feel', 3.48),
+  ('All of Me', 4.59);
